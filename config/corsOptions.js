f91ac5001d12722bfbf00516a3ade4f92140e581
Johnny Lee <johnnylee@Johnnys-MacBook-Air.local>
2024-09-10 19:13:49 +0800

    update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,17 @@
 import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
   credentials: true, // This allows cookies to be sent
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
-  optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
